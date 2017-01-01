7fca98998d3694e3bc227e39288e6417fe04cc1a
alvin137 <alvin1377@gmail.com>
2017-01-01 23:12:51 +0900

    GUI관련,

--- src/main/java/alvin137/testmod/proxy/CommonProxy.java
+++ src/main/java/alvin137/testmod/proxy/CommonProxy.java
@@ -1,16 +1,20 @@
 package alvin137.testmod.proxy;
 
 import alvin137.testmod.blocks.RegisterBlocks;
+import alvin137.testmod.blocks.tileentity.TileEntityGUITestBlock;
 import alvin137.testmod.items.RegisterItems;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 	public void preinit(FMLPreInitializationEvent e) {
 		RegisterItems.registerItem();
 		RegisterBlocks.registerBlock();
+		
+		GameRegistry.registerTileEntity(TileEntityGUITestBlock.class, "asdfasdf");
 	}
 
 	public void init(FMLInitializationEvent e) {
